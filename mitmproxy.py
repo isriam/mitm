d945e9881ce2e3c5f93617146a73562885cf007d
isriam <jeremywindsor@hotmail.com>
2024-12-18 08:34:08 -0700

    creation of mitm for inspection

---
+++ mitmproxy.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from mitmproxy import http
+# from mitmproxy import options
+# from mitmproxy import proxy
+# from mitmproxy.tools.dump import DumpMaster
+from mitmproxy.addons import core
+import json
+import os
+
+
+class Aboutv2:
+    def __init__(self):
+        complete = False
+
+    def response(flow: http.HTTPFlow) -> None:
+        if flow.response and flow.response.content:
+            # print(vars(flow.request))
+            # print(flow.request.path)
+            if '/ext/dragonsong/event/about_v2' in flow.request.path:
+                print('about_v2', flow.request.path)
+                about_v2_raw = flow.response.content.decode("utf-8")
+                about_v2 = json.loads(about_v2_raw)
+                with open("/root/.mitmproxy/wardragons/about_v2.txt", "w") as file:
+                    json.dump(about_v2, file)
+                os.chmod("/root/.mitmproxy/wardragons/about_v2.txt", 0o744)
+
+
+def start():
+    myaddon = Aboutv2()
+    opts = options.Options(listen_host='0.0.0.0', listen_port=3124)
+    pconf = proxy.config.ProxyConfig(opts)
+    m = DumpMaster(opts)
+    m.server = proxy.server.ProxyServer(pconf)
+    m.addons.add(myaddon)
+
+    try:
+        m.run()
+    except KeyboardInterrupt:
+        m.shutdown()
+
+
+start()
---
+++ proxyscript.py
@@ -0,0 +1,31 @@
+from mitmproxy import http
+import json
+import os
+
+
+def response(flow: http.HTTPFlow) -> None:
+    if flow.response and flow.response.content:
+        #print(vars(flow.request))
+        #print(flow.request.path)
+        if '/ext/dragonsong/event/about_v2' in flow.request.path:
+            print('about_v2', flow.request.path)
+            about_v2_raw = flow.response.content.decode("utf-8")
+            about_v2 = json.loads(about_v2_raw)
+            with open("/root/.mitmproxy/wardragons/about_v2.txt", "w") as file:
+                json.dump(about_v2, file)
+            os.chmod("/root/.mitmproxy/wardragons/about_v2.txt", 0o744)
+        if '/dragons/event/current' in flow.request.path:
+            print('params', flow.request.path)
+            params = flow.response.content.decode("utf-8")
+            #print(params)
+            with open("/root/.mitmproxy/wardragons/params.txt", "w") as file:
+                file.write(params)
+            os.chmod("/root/.mitmproxy/wardragons/params.txt", 0o744)
+        if '/ext/dragonsong/world/get_params' in flow.request.path:
+            print('world_params', flow.request.path)
+            world_params_raw = flow.response.content.decode("utf-8")
+            world_params = json.loads(world_params_raw)
+            #print(world_params)
+            with open("/root/.mitmproxy/wardragons/world_params.txt", "w") as file:
+                json.dump(world_params, file)
+            os.chmod("/root/.mitmproxy/wardragons/world_params.txt", 0o744)
