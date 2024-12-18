afc658b8273b8cbf4742d39ce3c090bb7e1bcac9
isriam <jeremywindsor@hotmail.com>
2024-12-18 09:16:57 -0700

    creation of mitm for inspection

--- proxyscript.py
+++ proxyscript.py
@@ -1,7 +1,10 @@
-from mitmproxy import http
+from mitmproxy import http, ctx
 import json
 import os
 
+about_completed = False
+params_completed = False
+world_completed = False
 
 def response(flow: http.HTTPFlow) -> None:
     if flow.response and flow.response.content:
@@ -14,6 +17,7 @@ def response(flow: http.HTTPFlow) -> None:
             with open("/root/.mitmproxy/wardragons/about_v2.txt", "w") as file:
                 json.dump(about_v2, file)
             os.chmod("/root/.mitmproxy/wardragons/about_v2.txt", 0o744)
+            about_completed = True
         if '/dragons/event/current' in flow.request.path:
             print('params', flow.request.path)
             params = flow.response.content.decode("utf-8")
@@ -21,6 +25,7 @@ def response(flow: http.HTTPFlow) -> None:
             with open("/root/.mitmproxy/wardragons/params.txt", "w") as file:
                 file.write(params)
             os.chmod("/root/.mitmproxy/wardragons/params.txt", 0o744)
+            params_completed = True
         if '/ext/dragonsong/world/get_params' in flow.request.path:
             print('world_params', flow.request.path)
             world_params_raw = flow.response.content.decode("utf-8")
@@ -29,3 +34,8 @@ def response(flow: http.HTTPFlow) -> None:
             with open("/root/.mitmproxy/wardragons/world_params.txt", "w") as file:
                 json.dump(world_params, file)
             os.chmod("/root/.mitmproxy/wardragons/world_params.txt", 0o744)
+            world_completed = True
+
+
+if about_completed is True and params_completed is True and world_completed is True:
+    ctx.master.shutdown()
