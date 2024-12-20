e02c43c1ed6c7fb9ecc7dc1ff3a104d58010fd5e
isriam <jeremywindsor@hotmail.com>
2024-12-20 09:46:17 -0700

    creation of mitm for inspection

--- mitm.py
+++ mitm.py
@@ -6,8 +6,8 @@ from mitmproxy.tools.dump import DumpMaster
 import os, sys, re, datetime, json
 
 class Addon(object):
-def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
 def request(self, flow):
     # examine request here
@@ -21,16 +21,15 @@ def response(self, flow):
 
 
 if __name__ == "__main__":
+    options = Options(listen_host='0.0.0.0', listen_port=8080)
+    m = DumpMaster(options, with_termlog=False, with_dumper=False)
+    config = ProxyConfig(options)
 
-options = Options(listen_host='0.0.0.0', listen_port=8080)
-m = DumpMaster(options, with_termlog=False, with_dumper=False)
-config = ProxyConfig(options)
+    m.server = ProxyServer(config)
+    m.addons.add(Addon())
 
-m.server = ProxyServer(config)
-m.addons.add(Addon())
-
-try:
-    print('Redirection active.')
-    m.run()
-except KeyboardInterrupt:
-    m.shutdown()
+    try:
+        print('Redirection active.')
+        m.run()
+    except KeyboardInterrupt:
+        m.shutdown()
