00112696da8afcf5137c21e111aeddaa70548271
isriam <jeremywindsor@hotmail.com>
2024-12-23 08:56:01 -0700

    creating webpage

--- chest.py
+++ chest.py
@@ -21,7 +21,7 @@ def callback():
                 form_dict = {idx: ele for idx, ele in enumerate(form_text)}
                 # print(form_dict)
             print(form_dict)
-            chest_type = form_dict.get(0, c_type_dict.get('gold'))
+            chest_type = form_dict.get(c_type_dict.get(0), c_type_dict.get('gold'))
             chest_num = form_dict.get(1, 10)
             print(chest_type, chest_num)
             output = main(type=chest_type, c_num=chest_num)
